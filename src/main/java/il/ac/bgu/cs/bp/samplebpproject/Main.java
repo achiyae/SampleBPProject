3eaf5082b22368ab690924a7bee91b61e649a4f2
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-08-17 17:43:04 +0300

    initial commit

--- src/main/java/il/ac/bgu/cs/bp/samplebpproject/Main.java
+++ src/main/java/il/ac/bgu/cs/bp/samplebpproject/Main.java
@@ -5,6 +5,7 @@ import il.ac.bgu.cs.bp.bpjs.internal.ScriptableUtils;
 import il.ac.bgu.cs.bp.bpjs.model.*;
 import il.ac.bgu.cs.bp.statespacemapper.GenerateAllTracesInspection;
 import il.ac.bgu.cs.bp.statespacemapper.StateSpaceMapper;
+import il.ac.bgu.cs.bp.statespacemapper.exports.DotExporter;
 import org.jgrapht.nio.Attribute;
 import org.jgrapht.nio.DefaultAttribute;
 import org.jgrapht.nio.dot.DOTExporter;
@@ -38,7 +39,10 @@ public class Main {
     System.out.println("// completed mapping the states graph");
     System.out.println(res.toString());
 
-    exportGraph(name, res);
+    System.out.println("// Export to GraphViz...");
+    var outputDir = "exports";
+    var path = Paths.get(outputDir, name + ".dot").toString();
+    new DotExporter(res,path,name).export();
 
     System.out.println("// done");
   }
@@ -50,84 +54,5 @@ public class Main {
     var paths = res.generatePaths(findSimplePathsOnly, maxPathLength);
     System.out.println(paths);
     return paths;
-  }
-
-  private static void exportGraph(String runName, GenerateAllTracesInspection.MapperResult res) throws IOException {
-    Function<GenerateAllTracesInspection.MapperEdge, Map<String, Attribute>> edgeAttributeProvider = e -> Map.of(
-        /*"Event", DefaultAttribute.createAttribute(dotSanitizer(e.event.toString())),
-        "Event_name", DefaultAttribute.createAttribute(e.event.name),
-        "Event_value", DefaultAttribute.createAttribute(dotSanitizer(Objects.toString(e.event.maybeData))),*/
-        "label", DefaultAttribute.createAttribute(dotSanitizer(e.event.toString()))
-    );
-    Function<GenerateAllTracesInspection.MapperVertex, Map<String, Attribute>> vertexAttributeProvider = v -> {
-      boolean startNode = v.equals(res.startNode);
-      boolean acceptingNode = res.acceptingStates.contains(v);
-      return Map.of(
-          "hash", DefaultAttribute.createAttribute(v.hashCode()),
-          "store", DefaultAttribute.createAttribute(dotSanitizer(getStore(v.bpss))),
-          "statements", DefaultAttribute.createAttribute(dotSanitizer(getStatments(v.bpss))),
-          "bthreads", DefaultAttribute.createAttribute(dotSanitizer(getBThreads(v.bpss))),
-          "shape", DefaultAttribute.createAttribute(startNode ? "none " : acceptingNode ? "doublecircle" : "circle")
-      );
-    };
-    Supplier<Map<String, Attribute>> graphAttributeProvider = () -> Map.of(
-        "name", DefaultAttribute.createAttribute("\"" + runName + "\""),
-        "run_date", DefaultAttribute.createAttribute("\"" + DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now()) + "\""),
-        "num_of_vertices", DefaultAttribute.createAttribute(res.states().size()),
-        "num_of_edges", DefaultAttribute.createAttribute(res.edges().size()),
-        "num_of_events", DefaultAttribute.createAttribute(res.events.size())
-    );
-
-    System.out.println("// Export to GraphViz...");
-    var outputDir = "exports";
-    Files.createDirectories(Paths.get(outputDir));
-    var path = Paths.get(outputDir, runName + ".dot");
-    var dotExporter = new DOTExporter<GenerateAllTracesInspection.MapperVertex, GenerateAllTracesInspection.MapperEdge>();
-    dotExporter.setEdgeAttributeProvider(edgeAttributeProvider);
-    dotExporter.setVertexAttributeProvider(vertexAttributeProvider);
-    dotExporter.setGraphAttributeProvider(graphAttributeProvider);
-    try (var out = new PrintStream(path.toString())) {
-      dotExporter.exportGraph(res.graph, out);
-    }
-  }
-
-  private static String getBThreads(BProgramSyncSnapshot bpss) {
-    return bpss.getBThreadSnapshots().stream()
-        .map(BThreadSyncSnapshot::getName)
-        .sorted()
-        .collect(joining(","));
-  }
-
-  private static String dotSanitizer(String in) {
-    return in
-        .replace("\r\n", "")
-        .replace("\n", "")
-        .replace("\"", "'")
-        .replace("JS_Obj ", "")
-//        .replaceAll("[\\. \\-+]", "_");
-        ;
-  }
-
-  private static String getStore(BProgramSyncSnapshot bpss) {
-    return bpss.getDataStore().entrySet().stream()
-        .map(entry -> "{" + ScriptableUtils.stringify(entry.getKey()) + "," + ScriptableUtils.stringify(entry.getValue()) + "}")
-        .sorted()
-        .collect(joining(",", "[", "]"));
-  }
-
-  private static String getStatments(BProgramSyncSnapshot bpss) {
-    return bpss.getBThreadSnapshots().stream()
-        .map(btss -> {
-          SyncStatement syst = btss.getSyncStatement();
-          return
-              "{name: " + btss.getName() + ", " +
-                  "isHot: " + syst.isHot() + ", " +
-                  "request: " + syst.getRequest().stream().map(BEvent::toString).collect(joining(",", "[", "]")) + ", " +
-                  "waitFor: " + syst.getWaitFor().toString() + ", " +
-                  "block: " + syst.getBlock().toString() + ", " +
-                  "interrupt: " + syst.getInterrupt().toString() + "}";
-        })
-        .sorted()
-        .collect(joining(",\n", "[", "]"));
   }
 }
