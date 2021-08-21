940d3720e49ba226ece4a5e1561fe4dd67ea0875
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-08-22 00:09:03 +0300

    Fix to DiningPhilosophers.js

--- src/main/resources/DiningPhilosophers.js
+++ src/main/resources/DiningPhilosophers.js
@@ -1,33 +1,63 @@
+/* global bp */
 function bthread(name, func) {
-  return bp.registerBThread(name, func)
+  bp.registerBThread(name, func)
 }
 
-function sync(stmt) {
+function sync(stmt, data) {
+  if (data)
+    return bp.sync(stmt, data)
   return bp.sync(stmt)
 }
 
-const take = i => bp.Event("take " + i)
-const put = i => bp.Event("put " + i)
-
 const PHILOSOPHER_COUNT = 2
 
-for (let i = 0; i < PHILOSOPHER_COUNT; i++) {
-  // Add fork i behavior:
-  bthread("Fork" + i, function () {
+const Take = (i, side) => bp.Event('Take', {id: i, side: side})
+const Put = (i, side) => bp.Event('Put', {id: i, side: side})
+const TakeSemaphore = i => bp.Event('TakeSemaphore', i)
+const ReleaseSemaphore = i => bp.Event('ReleaseSemaphore', i)
+const AnyTakeSemaphore = ()=>bp.EventSet('AnyTakeSemaphore', function (e) {
+  return e.name.equals(String('TakeSemaphore'))
+})
+const AnyReleaseSemaphore = ()=>bp.EventSet('AnyReleaseSemaphore', function (e) {
+  return e.name.equals(String('ReleaseSemaphore'))
+})
+const AnyTake = (i) => [Take(i, "R"), Take((i % PHILOSOPHER_COUNT) + 1, "L")]
+const AnyPut = (i) => [Put(i, "R"), Put((i % PHILOSOPHER_COUNT) + 1, "L")]
+
+function bt(i) {
+  bthread('Stick ' + i, function () {
     while (true) {
-      sync({waitFor: take(i), block: put(i)})
-      sync({waitFor: put(i), block: take(i)})
+      bp.sync({waitFor: AnyTake(i), block: AnyPut(i)});
+      bp.sync({waitFor: AnyPut(i), block: AnyTake(i)});
     }
   })
 
-  // Add philosopher i behavior:
-  let j = (i + 1) % PHILOSOPHER_COUNT
-  bthread("Philosopher" + i, function () {
+  bthread('Philosopher ' + i, function () {
     while (true) {
-      sync({request: take(i)})
-      sync({request: take(j)})
-      sync({request: put(j)})
-      sync({request: put(i)})
+      // Request to pick the right stick
+      sync({request: Take(i, 'R')});
+      sync({request: Take(i, 'L')});
+      sync({request: Put(i, 'L')});
+      sync({request: Put(i, 'R')});
+    }
+  })
+
+  bthread('Take semaphore '+i, function () {
+    while (true) {
+      sync({request: TakeSemaphore(i), block: Take(i, 'R')})
+      sync({waitFor: Put(i, 'R')})
+      sync({request: ReleaseSemaphore(i)})
     }
   })
 }
+
+for (let c = 0; c < PHILOSOPHER_COUNT; c++) {
+  bt(c)
+}
+
+bthread('Semaphore', function () {
+  while (true) {
+    sync({waitFor: AnyTakeSemaphore()})
+    sync({waitFor: AnyReleaseSemaphore(), block: AnyTakeSemaphore()})
+  }
+})
