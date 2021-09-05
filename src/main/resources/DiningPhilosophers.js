25cf3fa2f9683e67b936e1f5dbc1eb940c48f81d
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-09-05 13:11:03 +0300

    DiningPhilosophers.js

--- src/main/resources/DiningPhilosophers.js
+++ src/main/resources/DiningPhilosophers.js
@@ -15,53 +15,57 @@ function hot(isHot) {
 
 // ############ Basic behaviors ##############
 
-const PHILOSOPHER_COUNT = 3
+const PHIL_COUNT = 5
 
 const Take = (i, side) => bp.Event('Take', {id: i, side: side})
 const Put = (i, side) => bp.Event('Put', {id: i, side: side})
-const AnyTake = i => [Take(i, "R"), Take((i % PHILOSOPHER_COUNT) + 1, "L")]
-const AnyPut = i => [Put(i, "R"), Put((i % PHILOSOPHER_COUNT) + 1, "L")]
+const PhilosopherTakeEvent = i => [Take(i, 'R'), Take(i, 'L')]
+const PhilosopherPutEvent = i => [Put(i, 'R'), Put(i, 'L')]
+const AnyPhilosopherAction = i => PhilosopherTakeEvent(i).concat(PhilosopherPutEvent(i))
+const StickTaken = i => [Take(i, "R"), Take((i % PHIL_COUNT) + 1, "L")]
+const StickPut = i => [Put(i, "R"), Put((i % PHIL_COUNT) + 1, "L")]
 
-for (let c = 1; c <= PHILOSOPHER_COUNT; c++) {
+for (let c = 1; c <= PHIL_COUNT; c++) {
   let i = c
-  bthread('Stick ' + i, function () {
+  bthread('Fork ' + i + ' behavior', function () {
     while (true) {
-      bp.sync({waitFor: AnyTake(i), block: AnyPut(i)});
-      bp.sync({waitFor: AnyPut(i), block: AnyTake(i)});
+      sync({waitFor: StickTaken(i), block: StickPut(i)})
+      sync({waitFor: StickPut(i), block: StickTaken(i)})
     }
   })
 
-  bthread('Philosopher ' + i, function () {
+  bthread('Philosopher ' + i + ' behavior', function () {
     while (true) {
-      // Request to pick the right stick
-      sync({request: [Take(i, 'R'), Take(i, 'L')]});
-      sync({request: [Take(i, 'R'), Take(i, 'L')]});
-      sync({request: [Put(i, 'R'), Put(i, 'L')]});
-      sync({request: [Put(i, 'R'), Put(i, 'L')]});
+      sync({request: [Take(i, 'R'), Take(i, 'L')]})
+      sync({request: [Take(i, 'R'), Take(i, 'L')]})
+      sync({request: [Put(i, 'R'), Put(i, 'L')]})
+      sync({request: [Put(i, 'R'), Put(i, 'L')]})
     }
   })
 }
 
 
 // ############  Liveness requirements  ##############
-for (let c = 1; c <= PHILOSOPHER_COUNT; c++) {
+for (let c = 1; c <= PHIL_COUNT; c++) {
   let i = c
 
   // A taken stick will eventually be released
   bthread("[](take -> <>put)", function () {
     while (true) {
-      sync({waitFor: AnyTake(i)});
-      hot(true).sync({waitFor: AnyPut(i)})
+      sync({waitFor: StickTaken(i)})
+      hot(true).sync({waitFor: StickPut(i)})
     }
   })
 
   // A hungry philosopher will eventually eat
-  /*bthread("NoStarvation", function () {
+  bthread("NoStarvation", function () {
     while (true) {
-      hot(true).sync({waitFor: Take(i, 'L')})
-      sync({waitFor: Put(i, 'R')})
+      hot(true).sync({waitFor: [Take(i, 'R'), Take(i, 'L')]})
+      hot(true).sync({waitFor: [Take(i, 'R'), Take(i, 'L')]})
+      sync({waitFor: [Put(i, 'R'), Put(i, 'L')]})
+      sync({waitFor: [Put(i, 'R'), Put(i, 'L')]})
     }
-  })*/
+  })
 }
 
 
@@ -75,10 +79,6 @@ const AnyReleaseSemaphore = bp.EventSet('AnyReleaseSemaphore', function (e) {
   return e.name == 'ReleaseSemaphore'
 })
 
-const AnyPhilosopherAction = i => bp.EventSet('AnyPhilosopherAction ' + i, function (e) {
-  return ['Take', 'Put'].includes(String(e.name)) && e.data.id === i
-})
-
 bthread('Semaphore', function () {
   while (true) {
     sync({waitFor: AnyTakeSemaphore})
@@ -86,15 +86,24 @@ bthread('Semaphore', function () {
   }
 })
 
-for (let c = 1; c <= PHILOSOPHER_COUNT; c++) {
+for (let c = 1; c <= PHIL_COUNT; c++) {
   let i = c
-  let es = AnyPhilosopherAction(i)
+  /*bthread('Take semaphore ' + i, function () {
+    while (true) {
+      sync({request: TakeSemaphore(i), block: AnyPhilosopherAction(i)})
+      sync({waitFor: AnyPhilosopherAction(i)})
+      sync({waitFor: AnyPhilosopherAction(i)})
+      sync({request: ReleaseSemaphore(i), block: AnyPhilosopherAction(i)})
+      // sync({waitFor: AnyPhilosopherAction(i)})
+      // sync({waitFor: AnyPhilosopherAction(i)})
+    }
+  })*/
   bthread('Take semaphore ' + i, function () {
     while (true) {
-      sync({request: TakeSemaphore(i), block: es})
-      sync({waitFor: es})
-      sync({waitFor: es})
-      sync({request: ReleaseSemaphore(i), block: es})
+      sync({request: TakeSemaphore(i), block: [Take(i, 'R'),Take(i, 'L')]})
+      sync({waitFor: [Put(i, 'R'), Put(i, 'L')]})
+      sync({waitFor: [Put(i, 'R'), Put(i, 'L')]})
+      sync({request: ReleaseSemaphore(i), block: [Take(i, 'R'),Take(i, 'L')]})
     }
   })
 }
