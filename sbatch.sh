e217fef66545e61a37cb225b5f50b3c32ecfd2f8
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-09-21 17:33:25 +0300

    slurm updates

--- sbatch.sh
+++ sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #files=("lc_bp_v1" "lc_pn_check" "lc_bp_v1_faults" "lc_pn_check_faults")
-files=("lc_bp_v1"  "lc_pn_check")
+files=("lc_bp_v1" "lc_pn_check")
 N=6
 
 for file in ${files[@]}; do
