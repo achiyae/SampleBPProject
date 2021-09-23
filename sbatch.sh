24776900883cf8f4cbc259c8399ff678163bb329
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-09-23 22:04:38 +0300

    Add compressing code

--- sbatch.sh
+++ sbatch.sh
@@ -1,10 +1,14 @@
 #!/bin/bash 
 #files=("lc_bp_v1" "lc_pn_check" "lc_bp_v1_faults" "lc_pn_check_faults")
 files=("lc_bp_v1" "lc_pn_check")
-N=6
+N=4
 
 for file in ${files[@]}; do
   for i in $(seq 1 $N); do
-    sbatch "./job.sh" "levelCrossing/$file" $i
+    if [ $i -lt 3 ]; then
+      sbatch "./job.sh" "levelCrossing/$file" $i
+    else
+      sbatch "./job.sh" "levelCrossing/$file" $i 10
+    fi
   done
 done
--- src/main/java/il/ac/bgu/cs/bp/samplebpproject/levelCrossing/LevelCrossingMain.java
+++ src/main/java/il/ac/bgu/cs/bp/samplebpproject/levelCrossing/LevelCrossingMain.java
@@ -5,58 +5,67 @@ import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 import il.ac.bgu.cs.bp.bpjs.model.ResourceBProgram;
 import il.ac.bgu.cs.bp.statespacemapper.MapperResult;
 import il.ac.bgu.cs.bp.statespacemapper.StateSpaceMapper;
+import il.ac.bgu.cs.bp.statespacemapper.jgrapht.MapperEdge;
+import il.ac.bgu.cs.bp.statespacemapper.jgrapht.MapperVertex;
 import il.ac.bgu.cs.bp.statespacemapper.jgrapht.exports.DotExporter;
+import org.jgrapht.Graph;
 import org.jgrapht.nio.DefaultAttribute;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LevelCrossingMain {
-  // Program arguments = args[0] = filename without ".js", args[1] = number of railways, args[2] (optional) = max path length
+  // Program arguments =
+  //    args[0] = lc_bp | lc_pn | lc_bp_faults | lc_pn_faults
+  //    args[1] = number of railways
+  //    args[2] (optional) = max path length
   // For example: args = ["lc_pn_check", "1"]
   public static void main(String[] args) throws Exception {
     System.out.println("Run name: " + args[0] + "_" + args[1]);
 
     var railways = Integer.parseInt(args[1]);
-    var filename = args[0] + ".js";
+    var filename = "levelCrossing/" + args[0] + ".js";
     var runName = args[0] + "_R-" + railways;
+    var csvName = runName + ".csv";
+    var outputDir = "exports";
     Integer maxPathLength = null;
     if (args.length == 3) {
       maxPathLength = Integer.valueOf(args[2]);
-      runName += "_L-" + maxPathLength;
+      csvName = runName + "_L-" + maxPathLength + ".csv";
     }
     final BProgram bprog = new ResourceBProgram(filename);
     bprog.putInGlobalScope("n", railways);
 
     printJVMStats();
 
-    // You can use a different EventSelectionStrategy, for example:
-    /* var ess = new PrioritizedBSyncEventSelectionStrategy();
-    bprog.setEventSelectionStrategy(ess); */
     System.out.println("// Start mapping the states graph");
-
     var res = new StateSpaceMapper().mapSpace(bprog);
     System.out.println("// Completed mapping the states graph");
     System.out.println(res.toString());
 
-    System.out.println("// Export to GraphViz...");
-    var outputDir = "exports";
-    var path = Paths.get(outputDir, runName + ".dot").toString();
-    var exporter = new DotExporter(res, path, runName);
-    exporter.setVertexAttributeProvider(v -> Map.of()
-//        Map.of("hash", DefaultAttribute.createAttribute(v.hashCode()))
-    );
-    exporter.setEdgeAttributeProvider(v -> Map.of(
-        "label", DefaultAttribute.createAttribute(v.event.name)
-    ));
-    exporter.export();
+    exportGraph(outputDir, runName, res);
 
+    if (runName.startsWith("lc_pn")) {
+      res = PNMapperResults.Create(res);
+      exportGraph(outputDir, runName + "_compressed", res);
+    }
+
+    generatePaths(csvName, maxPathLength, res, outputDir);
+
+    System.out.println("// done");
+  }
+
+  private static void generatePaths(String csvName, Integer maxPathLength, MapperResult res, String outputDir) throws IOException {
     System.out.println("// Generating paths...");
     var allDirectedPathsAlgorithm = res.createAllDirectedPathsBuilder()
-        .setSimplePathsOnly(true)
-        .setIncludeReturningEdgesInSimplePaths(true)
+        .setSimplePathsOnly(maxPathLength == null)
+        .setIncludeReturningEdgesInSimplePaths(false)
         .setLongestPathsOnly(false)
         .setMaxPathLength(maxPathLength)
         .build();
@@ -66,15 +75,27 @@ public class LevelCrossingMain {
         .stream()
         .map(l -> l.stream()
             .map(BEvent::getName)
-            .collect(Collectors.joining(", ")))
+            .filter(s -> !List.of("KeepDown", "ClosingRequest", "OpeningRequest").contains(s))
+            .collect(Collectors.joining(",")))
         .distinct()
         .sorted()
         .collect(Collectors.joining("\n"));
 
     System.out.println("// Writing paths...");
-    Files.writeString(Paths.get(outputDir, runName + ".csv"), paths);
+    Files.writeString(Paths.get(outputDir, csvName), paths);
+  }
 
-    System.out.println("// done");
+  private static void exportGraph(String outputDir, String runName, MapperResult res) throws IOException {
+    System.out.println("// Export to GraphViz...");
+    var path = Paths.get(outputDir, runName + ".dot").toString();
+    var exporter = new DotExporter(res, path, runName);
+    exporter.setVertexAttributeProvider(v -> Map.of()
+//        Map.of("hash", DefaultAttribute.createAttribute(v.hashCode()))
+    );
+    exporter.setEdgeAttributeProvider(v -> Map.of(
+        "label", DefaultAttribute.createAttribute(v.event.name)
+    ));
+    exporter.export();
   }
 
   private static void printJVMStats() {
@@ -94,5 +115,44 @@ public class LevelCrossingMain {
     /* Total memory currently in use by the JVM */
     System.out.println("Total memory (bytes): " +
         Runtime.getRuntime().totalMemory());
+  }
+
+  private static class PNMapperResults extends MapperResult {
+    private PNMapperResults(Graph<MapperVertex, MapperEdge> graph, MapperVertex startNode, Set<MapperVertex> acceptingStates) {
+      super(graph, startNode, acceptingStates);
+    }
+
+    public static PNMapperResults Create(MapperResult base) {
+      var graph = base.graph;
+      while (true) {
+        var edge = graph.edgeSet().parallelStream()
+            .filter(e -> List.of("KeepDown", "ClosingRequest", "OpeningRequest").contains(e.event.name))
+            .findAny().orElse(null);
+        if (edge == null) break;
+        var source = graph.getEdgeSource(edge);
+        var target = graph.getEdgeTarget(edge);
+        var sourceIn = new ArrayList<>(graph.incomingEdgesOf(source));
+        var sourceOut = new ArrayList<>(graph.outgoingEdgesOf(source));
+        for (var e : sourceIn) {
+          var eSource = graph.getEdgeSource(e);
+          // if there is no edge with this event between eSource and target then add it.
+          if (graph.getAllEdges(eSource, target).parallelStream().noneMatch(e1 -> e1.event.equals(e.event)))
+            graph.addEdge(eSource, target, new MapperEdge(e.event));
+          graph.removeEdge(e);
+        }
+        for (var e : sourceOut) {
+          if (e != edge) {
+            var eTarget = graph.getEdgeTarget(e);
+            // if there is no edge with this event between eTarget and target then add it.
+            if (graph.getAllEdges(target, eTarget).parallelStream().noneMatch(e1 -> e1.event.equals(e.event)))
+              graph.addEdge(target, eTarget, new MapperEdge(e.event));
+          }
+          graph.removeEdge(e);
+        }
+        graph.removeVertex(source);
+      }
+      var startNode = base.startNode;
+      return new PNMapperResults(graph, startNode, graph.vertexSet());
+    }
   }
 }
