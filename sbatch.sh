b5d30b6e5c523c1576dfccbebb95734c76244b2b
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-09-24 15:50:07 +0300

    Fixed bug in graph simplification

--- sbatch.sh
+++ sbatch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 #files=("lc_bp_v1" "lc_pn_check" "lc_bp_v1_faults" "lc_pn_check_faults")
-files=("lc_bp_v1" "lc_pn_check")
-N=4
+files=("lc_bp" "lc_pn")
+N=5
 
 for file in ${files[@]}; do
   for i in $(seq 1 $N); do
     if [ $i -lt 3 ]; then
-      sbatch "./job.sh" "levelCrossing/$file" $i
+      sbatch "./job.sh" $file $i
     else
-      sbatch "./job.sh" "levelCrossing/$file" $i 10
+      sbatch "./job.sh" $file $i 14
     fi
   done
 done
