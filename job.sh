8143cf413a7762049e0f4ee984b15f208b24f3fd
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-09-25 22:51:58 +0300

    fix maven shutting down threads exception

--- job.sh
+++ job.sh
@@ -3,8 +3,9 @@
 #SBATCH --partition main ### specify partition name where to run a job
 ##SBATCH --time 1-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
 #SBATCH --job-name sel_verification ### name of the job. replace my_job with your desired job name
-#SBATCH --output my_job-id-%J.out ### output log for running job - %J is the job number variable
-#SBATCH --error my_job-id-%J.err ### output log for running job - %J is the job number variable
+## no need to redirect out and err to different files. By default they are redirected to slurm-%j.out
+##SBATCH --output my_job-id-%J.out ### output log for running job - %J is the job number variable
+##SBATCH --error my_job-id-%J.err ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=achiya@bgu.ac.il ### users email for sending job status notifications – replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=500G ### total amount of RAM
