23a5b2832d7ca246ceab5ba529f54306094665c4
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-12-16 16:04:30 +0200

    Added Evolved TTT

--- src/main/resources/EvolvedTicTacToe/bl.js
+++ src/main/resources/EvolvedTicTacToe/bl.js
@@ -1,258 +1,98 @@
-function Move(row, col, type) {
-  return bp.Event(type, {row: row, col: col});
-}
-
-function X(row, col) {
-  return Move(row, col, "X");
-}
-
-function O(row, col) {
-  return Move(row, col, "O");
-}
-
-var move = bp.EventSet("Move events", function (e) {
-  //return e instanceof Move;
-  return e.name == 'X' || e.name == "O";
-});
-
-var Xmove = bp.EventSet("X events", function (e) {
-  //return e instanceof Move;
-  return e.name == "X"
-});
-
-var Omove = bp.EventSet("O events", function (e) {
-  //return e instanceof Move;
-  return e.name == "O"
-});
-
-var XWin = bp.Event("XWin");
-var OWin = bp.Event("OWin");
-var draw = bp.Event("Draw");
-var gameOver = bp.EventSet("GameOver", function (e) {
-  return e.equals(XWin) || e.equals(OWin) || e.equals(draw);
-});
-
-// GameRules:
-function addSquareBThreads(row, col) {
-  bp.registerBThread("RandomXPlayer(" + row + "," + col + ")", function () {
-    bp.sync({request: [X(row, col)]});
-  });
-  bp.registerBThread("SquareTaken(" + row + "," + col + ")", function () {
-    while (true) {
-      bp.sync({waitFor: [X(row, col), O(row, col)]});
-      bp.sync({block: [X(row, col), O(row, col)]});
-    }
-  });
-}
-
-for (var r = 0; r < 3; r++) {
-  for (var c = 0; c < 3; c++) {
-    addSquareBThreads(r, c);
-  }
-}
-bp.registerBThread("EnforceTurns", function () {
-  while (true) {
-    bp.sync({waitFor: Omove, block: Xmove});
-    bp.sync({waitFor: Xmove, block: Omove});
-  }
-});
-
-// Represents when the game ends
-bp.registerBThread("EndOfGame", function () {
-  bp.sync({waitFor: gameOver})
-  bp.sync({block: bp.all})
-});
-
-// Represents when it is a draw
-bp.registerBThread("DetectDraw", function () {
-  for (var i = 0; i < 9; i++) {
-    bp.sync({waitFor: move})
-  }
-  bp.sync({request: draw}, 90);
-});
-
-function addLinePermutationBthreads(l) {
-
-  // Represents when X wins
-  bp.registerBThread("DetectXWin(<" + l[0].x + "," + l[0].y + ">," + "<" + l[1].x + "," + l[1].y + ">," + "<" + l[2].x + "," + l[2].y + ">)", function () {
-    while (true) {
-      bp.sync({waitFor: [X(l[0].x, l[0].y), X(l[1].x, l[1].y), X(l[2].x, l[2].y)]});
-      bp.sync({waitFor: [X(l[0].x, l[0].y), X(l[1].x, l[1].y), X(l[2].x, l[2].y)]});
-      bp.sync({waitFor: [X(l[0].x, l[0].y), X(l[1].x, l[1].y), X(l[2].x, l[2].y)]});
-      bp.sync({ request: XWin }, 100);
-    }
+function behaviorSet0(input, index) {
+  bp.registerBThread("O_Player_Thread_0_0", function(){
+    bp.sync({waitFor:[O(input[3].x, input[3].y)]});
+    bp.sync({request:[O(input[3].x, input[3].y)]},9);
+    bp.sync({request: bp.Event("SET_0_THREAD_0_INPUT_" + index.toString())}, 115);
   });
 
-  // Represents when O wins
-  bp.registerBThread("DetectOWin(<" + l[0].x + "," + l[0].y + ">," + "<" + l[1].x + "," + l[1].y + ">," + "<" + l[2].x + "," + l[2].y + ">)", function () {
-    while (true) {
-      bp.sync({waitFor: [O(l[0].x, l[0].y), O(l[1].x, l[1].y), O(l[2].x, l[2].y)]});
-      bp.sync({waitFor: [O(l[0].x, l[0].y), O(l[1].x, l[1].y), O(l[2].x, l[2].y)]});
-      bp.sync({waitFor: [O(l[0].x, l[0].y), O(l[1].x, l[1].y), O(l[2].x, l[2].y)]});
-      bp.sync({ request: OWin }, 100);
-    }
+  bp.registerBThread("O_Player_Thread_0_1", function(){
+    bp.sync({waitFor:[X(input[3].x, input[3].y), O(input[0].x, input[0].y), X(input[2].x, input[2].y)]});
+    bp.sync({waitFor:[O(input[1].x, input[1].y), X(input[4].x, input[4].y), X(input[6].x, input[6].y)]});
+    bp.sync({request:[O(input[5].x, input[5].y)]},2);
+    bp.sync({request: bp.Event("SET_0_THREAD_1_INPUT_" + index.toString())}, 115);
   });
 
-  // Player O strategy to add a the third O to win
-  //LINE_CODE
 }
-
-// Player O strategy:
-
-var lines = [[{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}],
-  [{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}],
-  [{x: 2, y: 0}, {x: 2, y: 1}, {x: 2, y: 2}],
-  [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}],
-  [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}],
-  [{x: 0, y: 2}, {x: 1, y: 2}, {x: 2, y: 2}],
-  [{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2}],
-  [{x: 0, y: 2}, {x: 1, y: 1}, {x: 2, y: 0}]];
-
-lines.forEach(function (l) {
-  addLinePermutationBthreads(l);
+var inputs_0 = [[{x: 2, y: 2}, {x: 0, y: 2}, {x: 0, y: 1}, {x: 0, y: 1}, {x: 0, y: 1}, {x: 0, y: 1}, {x: 1, y: 2}],
+  [{x: 1, y: 1}, {x: 0, y: 1}, {x: 0, y: 1}, {x: 2, y: 1}, {x: 0, y: 2}, {x: 2, y: 0}, {x: 0, y: 2}],
+  [{x: 2, y: 2}, {x: 1, y: 2}, {x: 1, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 2, y: 0}],
+  [{x: 1, y: 2}, {x: 2, y: 0}, {x: 2, y: 1}, {x: 1, y: 1}, {x: 2, y: 2}, {x: 0, y: 0}, {x: 2, y: 1}],
+  [{x: 1, y: 0}, {x: 0, y: 0}, {x: 2, y: 1}, {x: 0, y: 2}, {x: 2, y: 0}, {x: 0, y: 2}, {x: 2, y: 0}],
+  [{x: 1, y: 2}, {x: 1, y: 1}, {x: 2, y: 2}, {x: 1, y: 1}, {x: 0, y: 0}, {x: 2, y: 1}, {x: 0, y: 2}],
+  [{x: 0, y: 2}, {x: 1, y: 2}, {x: 1, y: 2}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 2}, {x: 0, y: 0}],
+  [{x: 2, y: 2}, {x: 1, y: 2}, {x: 2, y: 2}, {x: 0, y: 1}, {x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 0}],
+  [{x: 0, y: 0}, {x: 0, y: 2}, {x: 1, y: 1}, {x: 0, y: 2}, {x: 2, y: 0}, {x: 2, y: 2}, {x: 2, y: 2}]]
+inputs_0.forEach(function (input, index) {
+  behaviorSet0(input, index);
 });
 
-
-var forks22 = [[{x: 1, y: 2}, {x: 2, y: 0}], [{x: 2, y: 1}, {x: 0, y: 2}], [{x: 1, y: 2}, {x: 2, y: 1}]];
-var forks02 = [[{x: 1, y: 2}, {x: 0, y: 0}], [{x: 0, y: 1}, {x: 2, y: 2}], [{x: 1, y: 2}, {x: 0, y: 1}]];
-var forks20 = [[{x: 1, y: 0}, {x: 2, y: 2}], [{x: 2, y: 1}, {x: 0, y: 0}], [{x: 2, y: 1}, {x: 1, y: 0}]];
-var forks00 = [[{x: 0, y: 1}, {x: 2, y: 0}], [{x: 1, y: 0}, {x: 0, y: 2}], [{x: 0, y: 1}, {x: 1, y: 0}]];
-
-var forksdiag = [[{x: 0, y: 2}, {x: 2, y: 0}],[ { x:0, y:0 },{ x:2, y:2 }] // <--- Intentional BUG
-];
-
-var permsforks = [[0, 1], [1, 0]];
-
-
-// Preference to put O on the center
-// O_PLAYER_CODE
-
-// Assertions
-
-function addAssertions(f, p) {
-
-  bp.registerBThread("AssertWin(<" + f[p[0]].x + "," + f[p[0]].y + ">," + "<" + f[p[1]].x + "," + f[p[1]].y + ">," + "<" + f[p[2]].x + "," + f[p[2]].y + ">)", function () {
-
-    //let ev1 = bp.sync({waitFor: [O(f[p[0]].x, f[p[0]].y), X(f[p[2]].x, f[p[2]].y)]})
-    if(bp.sync({waitFor: [O(f[p[0]].x, f[p[0]].y), X(f[p[2]].x, f[p[2]].y)]}).name.equals('X')) {
-      return;
-    }
-    //let ev2 = bp.sync({waitFor: [O(f[p[1]].x, f[p[1]].y), X(f[p[2]].x, f[p[2]].y)]});
-    if(bp.sync({waitFor: [O(f[p[1]].x, f[p[1]].y), X(f[p[2]].x, f[p[2]].y)]}).name.equals('X')){
-      return;
-    }
-    //if (ev2.name == 'X') {
-    //  return;
-    //}
-    //let ev = bp.sync({waitFor: [Omove, X(f[p[2]].x, f[p[2]].y)]});
-    while(true) {
-      if (bp.sync({waitFor: [Omove, X(f[p[2]].x, f[p[2]].y)]}).name.equals('X')) {
-        //x blocked the win - no violation
-        return;
-      } else {
-        if (bp.sync({waitFor: [OWin, Xmove]}).name.equals('X')) {
-          //x moved again, was not win
-          bp.sync({request: bp.Event("WIN_VIOLATION")}, 110);
-        } else {
-          //was a win
-          return;
-        }
-      }
-    }
-    //bp.ASSERT(ev.name.startsWith("X(") || ev.name.startsWith("O(" + f[p[2]].x + "," + f[p[2]].y + ")"), "WIN_VIOLATION");
-    /*if(!(ev.name.startsWith("X(") || ev.name.startsWith("O(" + f[p[2]].x + "," + f[p[2]].y + ")"))){
-      bp.sync({request: bp.Event("WIN_VIOLATION")}, 110);
-    }*/
+function behaviorSet1(input, index) {
+  bp.registerBThread("O_Player_Thread_1_0", function(){
+    bp.sync({waitFor:[X(input[1].x, input[1].y), X(input[1].x, input[1].y), X(input[0].x, input[0].y)]});
+    bp.sync({request:[O(input[1].x, input[1].y)]},9);
+    bp.sync({request: bp.Event("SET_1_THREAD_0_INPUT_" + index.toString())}, 115);
   });
 
-  bp.registerBThread("AssertBlock(<" + f[p[0]].x + "," + f[p[0]].y + ">," + "<" + f[p[1]].x + "," + f[p[1]].y + ">," + "<" + f[p[2]].x + "," + f[p[2]].y + ">)", function () {
-    //let ev1 = bp.sync({waitFor: [X(f[p[0]].x, f[p[0]].y), O(f[p[2]].x, f[p[2]].y)]});
-    if (bp.sync({waitFor: [X(f[p[0]].x, f[p[0]].y), O(f[p[2]].x, f[p[2]].y)]}).name.equals("O"))
-      return;
-
-    //let ev2 = bp.sync({waitFor: [X(f[p[1]].x, f[p[1]].y), O(f[p[2]].x, f[p[2]].y)]});
-    if (bp.sync({waitFor: [X(f[p[1]].x, f[p[1]].y), O(f[p[2]].x, f[p[2]].y)]}).name.equals("O"))
-      return;
-
-    // let ev = bp.sync({waitFor: [Omove]});
-    // let evNext = bp.sync({waitFor: [Xmove, OWin]})
-    // //bp.ASSERT(!evNext.name.startsWith("X(") || ev.name.startsWith("O(" + f[p[2]].x + "," + f[p[2]].y + ")"), "BLOCK_VIOLATION");
-    // if(!(!evNext.name.startsWith("X(") || ev.name.startsWith("O(" + f[p[2]].x + "," + f[p[2]].y + ")"))){
-    //   bp.sync({request: bp.Event("BLOCK_VIOLATION")}, 110);
-    // }
-
-
-    while (true) {
-      if (bp.sync({waitFor: [O(f[p[2]].x, f[p[2]].y), Xmove]}).name.equals('X')) {
-        //O was not placed to block
-        bp.sync({request: bp.Event("BLOCK_VIOLATION")}, 110);
-      } else {
-        //blocked win
-        bp.sync({request: bp.Event("BLOCK")}, 110);
-        return;
-      }
-    }
-  });
 }
+var inputs_1 = [[{x: 1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}],
+  [{x: 0, y: 1}, {x: 2, y: 2}, {x: 2, y: 0}],
+  [{x: 2, y: 2}, {x: 0, y: 2}, {x: 2, y: 0}],
+  [{x: 1, y: 2}, {x: 1, y: 0}, {x: 1, y: 1}],
+  [{x: 0, y: 2}, {x: 2, y: 0}, {x: 1, y: 0}],
+  [{x: 2, y: 2}, {x: 2, y: 0}, {x: 0, y: 1}],
+  [{x: 2, y: 2}, {x: 1, y: 1}, {x: 2, y: 0}],
+  [{x: 2, y: 1}, {x: 0, y: 1}, {x: 1, y: 2}],
+  [{x: 0, y: 2}, {x: 2, y: 2}, {x: 0, y: 1}]]
+inputs_1.forEach(function (input, index) {
+  behaviorSet1(input, index);
+});
 
-
-function addForkAssertions(f, solution) {
-
-  // Player X strategy to prevent the Fork22 of player O
-  bp.registerBThread("ForkAssertion(<" + f[0].x + "," + f[0].y + ">," + "<" + f[1].x + "," + f[1].y + ">)", function() {
-    while (true) {
-      solution.push(X(f[0].x, f[0].y));
-      solution.push(X(f[1].x, f[1].y));
-      if(bp.sync({ waitFor:solution }).name.equals("O"))
-        return;
-
-      if(bp.sync({ waitFor:solution }).name.equals("O"))
-        return;
-
-      solution.push(Xmove);
-      solution.push(OWin);
-
-      let ev = bp.sync({waitFor: solution});
-
-      if (ev.name.equals("OWin"))
-        return;
-      if (ev.name.equals("X"))
-        bp.sync({request: bp.Event("FORK_VIOLATION")}, 110);
-      else
-        bp.sync({request: bp.Event("FORK")}, 110);
-
-      // if (solution.includes(ev))
-      //   bp.sync({request: bp.Event("FORK")}, 110);
-      // if (ev.name.equals("OWin"))
-      //   return;
-      // else
-      //   bp.sync({request: bp.Event("FORK_VIOLATION")}, 110);
-    }
+function behaviorSet2(input, index) {
+  bp.registerBThread("O_Player_Thread_2_0", function(){
+    bp.sync({waitFor:[O(input[2].x, input[2].y), O(input[2].x, input[2].y), O(input[2].x, input[2].y), O(input[2].x, input[2].y)]});
+    bp.sync({request:[O(input[1].x, input[1].y)]},11);
+    bp.sync({request: bp.Event("SET_2_THREAD_0_INPUT_" + index.toString())}, 115);
   });
+
+  bp.registerBThread("O_Player_Thread_2_1", function(){
+    bp.sync({request:[O(input[2].x, input[2].y), O(input[2].x, input[2].y)]},7);
+    bp.sync({request: bp.Event("SET_2_THREAD_1_INPUT_" + index.toString())}, 115);
+  });
+
 }
+var inputs_2 = [[{x: 1, y: 2}, {x: 1, y: 0}, {x: 1, y: 0}],
+  [{x: 0, y: 0}, {x: 1, y: 1}, {x: 0, y: 0}],
+  [{x: 1, y: 2}, {x: 1, y: 2}, {x: 1, y: 1}],
+  [{x: 2, y: 2}, {x: 2, y: 2}, {x: 2, y: 2}],
+  [{x: 0, y: 1}, {x: 2, y: 1}, {x: 2, y: 0}],
+  [{x: 1, y: 1}, {x: 1, y: 0}, {x: 1, y: 2}],
+  [{x: 1, y: 2}, {x: 1, y: 2}, {x: 0, y: 2}],
+  [{x: 1, y: 1}, {x: 2, y: 2}, {x: 2, y: 2}]]
+inputs_2.forEach(function (input, index) {
+  behaviorSet2(input, index);
+});
 
-
-var perms = [[0, 1, 2], [0, 2, 1], [1, 0, 2],
-  [1, 2, 0], [2, 0, 1], [2, 1, 0]];
-
-lines.forEach(function (l) {
-  perms.forEach(function (p) {
-    addAssertions(l, p);
+function behaviorSet3(input, index) {
+  bp.registerBThread("O_Player_Thread_3_0", function(){
+    bp.sync({request:[O(input[4].x, input[4].y), O(input[4].x, input[4].y)]},10);
+    bp.sync({request: bp.Event("SET_3_THREAD_0_INPUT_" + index.toString())}, 115);
   });
-});
 
-forks22.forEach(function(f) {
-  addForkAssertions(f, [ O(2, 2), O(0,2), O(2,0) ]);
-});
-forks02.forEach(function(f) {
-  addForkAssertions(f, [ O(0, 2), O(0,0), O(2,2) ]);
-});
-forks20.forEach(function(f) {
-  addForkAssertions(f, [ O(2, 0), O(0,0), O(2,2) ]);
-});
-forks00.forEach(function(f) {
-  addForkAssertions(f, [ O(0, 0), O(0,2), O(2,0) ]);
-});
-forksdiag.forEach(function(f) {
-  addForkAssertions(f, [ O(0, 1), O(1, 0), O(1, 2), O(2, 1) ]);
+  bp.registerBThread("O_Player_Thread_3_1", function(){
+    bp.sync({waitFor:[X(input[2].x, input[2].y)]});
+    bp.sync({waitFor:[O(input[2].x, input[2].y), O(input[3].x, input[3].y), O(input[5].x, input[5].y), O(input[2].x, input[2].y)]});
+    bp.sync({request:[O(input[0].x, input[0].y)]},2);
+    bp.sync({request: bp.Event("SET_3_THREAD_1_INPUT_" + index.toString())}, 115);
+  });
+
+  bp.registerBThread("O_Player_Thread_3_2", function(){
+    bp.sync({waitFor:[X(input[1].x, input[1].y), O(input[4].x, input[4].y), X(input[1].x, input[1].y), X(input[2].x, input[2].y)]});
+    bp.sync({request:[O(input[1].x, input[1].y), O(input[2].x, input[2].y), O(input[1].x, input[1].y)]},9);
+    bp.sync({request: bp.Event("SET_3_THREAD_2_INPUT_" + index.toString())}, 115);
+  });
+
+}
+var inputs_3 = [[{x: 1, y: 0}, {x: 2, y: 2}, {x: 0, y: 2}, {x: 0, y: 1}, {x: 2, y: 2}, {x: 0, y: 1}]]
+inputs_3.forEach(function (input, index) {
+  behaviorSet3(input, index);
 });
--- src/main/resources/EvolvedTicTacToe/dal.js
+++ src/main/resources/EvolvedTicTacToe/dal.js
@@ -1,39 +1,46 @@
-function Click(row,col) {
-  return bp.Event("Click("+row+","+col+")");
+function Move(row, col, type) {
+  return bp.Event(type, {row: row, col: col});
 }
 
-function X(row,col) {
-  return bp.Event("X("+row+","+col+")");
+function X(row, col) {
+  return Move(row, col, "X");
 }
 
-function O(row,col) {
-  return bp.Event("O("+row+","+col+")");
+function O(row, col) {
+  return Move(row, col, "O");
 }
 
-StaticEvents={
-  'OWin':bp.Event("OWin"),
-  'XWin':bp.Event("XWin"),
-  'draw':bp.Event("draw")
-};
+var move = bp.EventSet("Move events", function (e) {
+  //return e instanceof Move;
+  return e.name == 'X' || e.name == "O";
+});
+
+var Xmove = bp.EventSet("X events", function (e) {
+  //return e instanceof Move;
+  return e.name == "X"
+});
+
+var Omove = bp.EventSet("O events", function (e) {
+  //return e instanceof Move;
+  return e.name == "O"
+});
+
+var XWin = bp.Event("XWin");
+var OWin = bp.Event("OWin");
+var draw = bp.Event("Draw");
+var gameOver = bp.EventSet("GameOver", function (e) {
+  return e.equals(XWin) || e.equals(OWin) || e.equals(draw);
+});
 
 // GameRules:
-
-// This BThreads are on each square of the grid
 function addSquareBThreads(row, col) {
-
-  // Detects mouse click
-  bp.registerBThread("ClickHandler(" + row + "," + col + ")", function() {
-    while (true) {
-      bp.sync({ waitFor: Click(row, col)  });
-      bp.sync({ request:[ X(row, col) ] });
-    }
+  bp.registerBThread("RandomXPlayer(" + row + "," + col + ")", function () {
+    bp.sync({request: [X(row, col)]});
   });
-
-  // Blocks further marking of a square already marked by X or O.
-  bp.registerBThread("SquareTaken(" + row + "," + col + ")", function() {
+  bp.registerBThread("SquareTaken(" + row + "," + col + ")", function () {
     while (true) {
-      bp.sync({ waitFor:[ X(row, col), O(row, col) ] });
-      bp.sync({ block:[ X(row, col), O(row, col) ] });
+      bp.sync({waitFor: [X(row, col), O(row, col)]});
+      bp.sync({block: [X(row, col), O(row, col)]});
     }
   });
 }
@@ -43,101 +50,209 @@ for (var r = 0; r < 3; r++) {
     addSquareBThreads(r, c);
   }
 }
-
-// Represents Enforce Turns
-bp.registerBThread("EnforceTurns", function() {
+bp.registerBThread("EnforceTurns", function () {
   while (true) {
-    bp.sync({ waitFor:[ X(0, 0), X(0, 1), X(0, 2),
-        X(1, 0), X(1, 1), X(1, 2),
-        X(2, 0), X(2, 1), X(2, 2) ],
-      block:[ O(0, 0), O(0, 1), O(0, 2),
-        O(1, 0), O(1, 1), O(1, 2),
-        O(2, 0), O(2, 1), O(2, 2) ] });
-    bp.sync({ waitFor:[ O(0, 0), O(0, 1), O(0, 2), O(1, 0), O(1, 1), O(1, 2), O(2, 0), O(2, 1), O(2, 2) ],
-      block:[ X(0, 0), X(0, 1), X(0, 2), X(1, 0), X(1, 1), X(1, 2), X(2, 0), X(2, 1), X(2, 2) ] });
+    bp.sync({waitFor: Omove, block: Xmove});
+    bp.sync({waitFor: Xmove, block: Omove});
   }
 });
 
 // Represents when the game ends
-bp.registerBThread("EndOfGame", function() {
-  bp.sync({ waitFor:[ StaticEvents.OWin, StaticEvents.XWin, StaticEvents.draw ] });
-  bp.sync({ block:[ X(0, 0), X(0, 1), X(0, 2),
-      X(1, 0), X(1, 1), X(1, 2),
-      X(2, 0), X(2, 1), X(2, 2),
-      O(0, 0), O(0, 1), O(0, 2),
-      O(1, 0), O(1, 1), O(1, 2),
-      O(2, 0), O(2, 1), O(2, 2) ] });
-});
-
-var move = bp.EventSet("Move events", function(e) {
-  return e.name.startsWith("O") || e.name.startsWith("X");
+bp.registerBThread("EndOfGame", function () {
+  bp.sync({waitFor: gameOver})
+  bp.sync({block: bp.all})
 });
 
 // Represents when it is a draw
-bp.registerBThread("DetectDraw", function() {
-  // For debug
-  bp.sync({ waitFor:[ move ] });
-  bp.sync({ waitFor:[ move ] });
-  bp.sync({ waitFor:[ move ] });
-
-  bp.sync({ waitFor:[ move ] });
-  bp.sync({ waitFor:[ move ] });
-  bp.sync({ waitFor:[ move ] });
-
-  bp.sync({ waitFor:[ move ] });
-  bp.sync({ waitFor:[ move ] });
-  bp.sync({ waitFor:[ move ] });
-  /*
-   * for (var i=0; i< 9; i++) { bp.sync({ waitFor:[ move ] }); }
-   */
-
-  bp.sync({ request:[ StaticEvents.draw ] }, 90);
+bp.registerBThread("DetectDraw", function () {
+  for (var i = 0; i < 9; i++) {
+    bp.sync({waitFor: move})
+  }
+  bp.sync({request: draw}, 90);
 });
 
-function addLinePermutationBthreads(l, p) {
+function addLinePermutationBthreads(l) {
 
   // Represents when X wins
-  bp.registerBThread("DetectXWin(<" + l[p[0]].x + "," + l[p[0]].y + ">," + "<" + l[p[1]].x + "," + l[p[1]].y + ">," + "<" + l[p[2]].x + "," + l[p[2]].y + ">)", function () {
+  bp.registerBThread("DetectXWin(<" + l[0].x + "," + l[0].y + ">," + "<" + l[1].x + "," + l[1].y + ">," + "<" + l[2].x + "," + l[2].y + ">)", function () {
     while (true) {
-      bp.sync({ waitFor: [X(l[p[0]].x, l[p[0]].y)] });
-
-      bp.sync({ waitFor: [X(l[p[1]].x, l[p[1]].y)] });
-
-      bp.sync({ waitFor: [X(l[p[2]].x, l[p[2]].y)] });
-
-      bp.sync({ request: [StaticEvents.XWin] }, 100);
-
+      bp.sync({waitFor: [X(l[0].x, l[0].y), X(l[1].x, l[1].y), X(l[2].x, l[2].y)]});
+      bp.sync({waitFor: [X(l[0].x, l[0].y), X(l[1].x, l[1].y), X(l[2].x, l[2].y)]});
+      bp.sync({waitFor: [X(l[0].x, l[0].y), X(l[1].x, l[1].y), X(l[2].x, l[2].y)]});
+      bp.sync({ request: XWin }, 100);
     }
   });
 
   // Represents when O wins
-  bp.registerBThread("DetectOWin(<" + l[p[0]].x + "," + l[p[0]].y + ">," + "<" + l[p[1]].x + "," + l[p[1]].y + ">," + "<" + l[p[2]].x + "," + l[p[2]].y + ">)", function () {
+  bp.registerBThread("DetectOWin(<" + l[0].x + "," + l[0].y + ">," + "<" + l[1].x + "," + l[1].y + ">," + "<" + l[2].x + "," + l[2].y + ">)", function () {
     while (true) {
-      bp.sync({ waitFor: [O(l[p[0]].x, l[p[0]].y)] });
+      bp.sync({waitFor: [O(l[0].x, l[0].y), O(l[1].x, l[1].y), O(l[2].x, l[2].y)]});
+      bp.sync({waitFor: [O(l[0].x, l[0].y), O(l[1].x, l[1].y), O(l[2].x, l[2].y)]});
+      bp.sync({waitFor: [O(l[0].x, l[0].y), O(l[1].x, l[1].y), O(l[2].x, l[2].y)]});
+      bp.sync({ request: OWin }, 100);
+    }
+  });
 
-      bp.sync({ waitFor: [O(l[p[1]].x, l[p[1]].y)] });
+  // Player O strategy to add a the third O to win
+  //LINE_CODE
+}
 
-      bp.sync({ waitFor: [O(l[p[2]].x, l[p[2]].y)] });
+// Player O strategy:
 
-      bp.sync({ request: [StaticEvents.OWin] }, 100);
+var lines = [[{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}],
+  [{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}],
+  [{x: 2, y: 0}, {x: 2, y: 1}, {x: 2, y: 2}],
+  [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}],
+  [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}],
+  [{x: 0, y: 2}, {x: 1, y: 2}, {x: 2, y: 2}],
+  [{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2}],
+  [{x: 0, y: 2}, {x: 1, y: 1}, {x: 2, y: 0}]];
 
+lines.forEach(function (l) {
+  addLinePermutationBthreads(l);
+});
+
+
+var forks22 = [[{x: 1, y: 2}, {x: 2, y: 0}], [{x: 2, y: 1}, {x: 0, y: 2}], [{x: 1, y: 2}, {x: 2, y: 1}]];
+var forks02 = [[{x: 1, y: 2}, {x: 0, y: 0}], [{x: 0, y: 1}, {x: 2, y: 2}], [{x: 1, y: 2}, {x: 0, y: 1}]];
+var forks20 = [[{x: 1, y: 0}, {x: 2, y: 2}], [{x: 2, y: 1}, {x: 0, y: 0}], [{x: 2, y: 1}, {x: 1, y: 0}]];
+var forks00 = [[{x: 0, y: 1}, {x: 2, y: 0}], [{x: 1, y: 0}, {x: 0, y: 2}], [{x: 0, y: 1}, {x: 1, y: 0}]];
+
+var forksdiag = [[{x: 0, y: 2}, {x: 2, y: 0}],[ { x:0, y:0 },{ x:2, y:2 }] // <--- Intentional BUG
+];
+
+var permsforks = [[0, 1], [1, 0]];
+
+
+// Preference to put O on the center
+// O_PLAYER_CODE
+
+// Assertions
+
+function addAssertions(f, p) {
+
+  bp.registerBThread("AssertWin(<" + f[p[0]].x + "," + f[p[0]].y + ">," + "<" + f[p[1]].x + "," + f[p[1]].y + ">," + "<" + f[p[2]].x + "," + f[p[2]].y + ">)", function () {
+
+    //let ev1 = bp.sync({waitFor: [O(f[p[0]].x, f[p[0]].y), X(f[p[2]].x, f[p[2]].y)]})
+    if(bp.sync({waitFor: [O(f[p[0]].x, f[p[0]].y), X(f[p[2]].x, f[p[2]].y)]}).name.equals('X')) {
+      return;
+    }
+    //let ev2 = bp.sync({waitFor: [O(f[p[1]].x, f[p[1]].y), X(f[p[2]].x, f[p[2]].y)]});
+    if(bp.sync({waitFor: [O(f[p[1]].x, f[p[1]].y), X(f[p[2]].x, f[p[2]].y)]}).name.equals('X')){
+      return;
+    }
+    //if (ev2.name == 'X') {
+    //  return;
+    //}
+    //let ev = bp.sync({waitFor: [Omove, X(f[p[2]].x, f[p[2]].y)]});
+    while(true) {
+      if (bp.sync({waitFor: [Omove, X(f[p[2]].x, f[p[2]].y)]}).name.equals('X')) {
+        //x blocked the win - no violation
+        return;
+      } else {
+        if (bp.sync({waitFor: [OWin, Xmove]}).name.equals('X')) {
+          //x moved again, was not win
+          bp.sync({request: bp.Event("WIN_VIOLATION")}, 110);
+        } else {
+          //was a win
+          return;
+        }
+      }
+    }
+    //bp.ASSERT(ev.name.startsWith("X(") || ev.name.startsWith("O(" + f[p[2]].x + "," + f[p[2]].y + ")"), "WIN_VIOLATION");
+    /*if(!(ev.name.startsWith("X(") || ev.name.startsWith("O(" + f[p[2]].x + "," + f[p[2]].y + ")"))){
+      bp.sync({request: bp.Event("WIN_VIOLATION")}, 110);
+    }*/
+  });
+
+  bp.registerBThread("AssertBlock(<" + f[p[0]].x + "," + f[p[0]].y + ">," + "<" + f[p[1]].x + "," + f[p[1]].y + ">," + "<" + f[p[2]].x + "," + f[p[2]].y + ">)", function () {
+    //let ev1 = bp.sync({waitFor: [X(f[p[0]].x, f[p[0]].y), O(f[p[2]].x, f[p[2]].y)]});
+    if (bp.sync({waitFor: [X(f[p[0]].x, f[p[0]].y), O(f[p[2]].x, f[p[2]].y)]}).name.equals("O"))
+      return;
+
+    //let ev2 = bp.sync({waitFor: [X(f[p[1]].x, f[p[1]].y), O(f[p[2]].x, f[p[2]].y)]});
+    if (bp.sync({waitFor: [X(f[p[1]].x, f[p[1]].y), O(f[p[2]].x, f[p[2]].y)]}).name.equals("O"))
+      return;
+
+    // let ev = bp.sync({waitFor: [Omove]});
+    // let evNext = bp.sync({waitFor: [Xmove, OWin]})
+    // //bp.ASSERT(!evNext.name.startsWith("X(") || ev.name.startsWith("O(" + f[p[2]].x + "," + f[p[2]].y + ")"), "BLOCK_VIOLATION");
+    // if(!(!evNext.name.startsWith("X(") || ev.name.startsWith("O(" + f[p[2]].x + "," + f[p[2]].y + ")"))){
+    //   bp.sync({request: bp.Event("BLOCK_VIOLATION")}, 110);
+    // }
+
+
+    while (true) {
+      if (bp.sync({waitFor: [O(f[p[2]].x, f[p[2]].y), Xmove]}).name.equals('X')) {
+        //O was not placed to block
+        bp.sync({request: bp.Event("BLOCK_VIOLATION")}, 110);
+      } else {
+        //blocked win
+        bp.sync({request: bp.Event("BLOCK")}, 110);
+        return;
+      }
     }
   });
 }
 
-var lines = [ [ { x:0, y:0 }, { x:0, y:1 }, { x:0, y:2 } ],
-  [ { x:1, y:0 }, { x:1, y:1 }, { x:1, y:2 } ],
-  [ { x:2, y:0 }, { x:2, y:1 }, { x:2, y:2 } ],
-  [ { x:0, y:0 }, { x:1, y:0 }, { x:2, y:0 } ],
-  [ { x:0, y:1 }, { x:1, y:1 }, { x:2, y:1 } ],
-  [ { x:0, y:2 }, { x:1, y:2 }, { x:2, y:2 } ],
-  [ { x:0, y:0 }, { x:1, y:1 }, { x:2, y:2 } ],
-  [ { x:0, y:2 }, { x:1, y:1 }, { x:2, y:0 } ] ];
-var perms = [ [ 0, 1, 2 ], [ 0, 2, 1 ], [ 1, 0, 2 ],
-  [ 1, 2, 0 ], [ 2, 0, 1 ], [ 2, 1, 0 ] ];
 
-lines.forEach(function(l) {
-  perms.forEach(function(p) {
-    addLinePermutationBthreads(l, p);
+function addForkAssertions(f, solution) {
+
+  // Player X strategy to prevent the Fork22 of player O
+  bp.registerBThread("ForkAssertion(<" + f[0].x + "," + f[0].y + ">," + "<" + f[1].x + "," + f[1].y + ">)", function() {
+    while (true) {
+      solution.push(X(f[0].x, f[0].y));
+      solution.push(X(f[1].x, f[1].y));
+      if(bp.sync({ waitFor:solution }).name.equals("O"))
+        return;
+
+      if(bp.sync({ waitFor:solution }).name.equals("O"))
+        return;
+
+      solution.push(Xmove);
+      solution.push(OWin);
+
+      let ev = bp.sync({waitFor: solution});
+
+      if (ev.name.equals("OWin"))
+        return;
+      if (ev.name.equals("X"))
+        bp.sync({request: bp.Event("FORK_VIOLATION")}, 110);
+      else
+        bp.sync({request: bp.Event("FORK")}, 110);
+
+      // if (solution.includes(ev))
+      //   bp.sync({request: bp.Event("FORK")}, 110);
+      // if (ev.name.equals("OWin"))
+      //   return;
+      // else
+      //   bp.sync({request: bp.Event("FORK_VIOLATION")}, 110);
+    }
   });
+}
+
+
+var perms = [[0, 1, 2], [0, 2, 1], [1, 0, 2],
+  [1, 2, 0], [2, 0, 1], [2, 1, 0]];
+
+lines.forEach(function (l) {
+  perms.forEach(function (p) {
+    addAssertions(l, p);
+  });
+});
+
+forks22.forEach(function(f) {
+  addForkAssertions(f, [ O(2, 2), O(0,2), O(2,0) ]);
+});
+forks02.forEach(function(f) {
+  addForkAssertions(f, [ O(0, 2), O(0,0), O(2,2) ]);
+});
+forks20.forEach(function(f) {
+  addForkAssertions(f, [ O(2, 0), O(0,0), O(2,2) ]);
+});
+forks00.forEach(function(f) {
+  addForkAssertions(f, [ O(0, 0), O(0,2), O(2,0) ]);
+});
+forksdiag.forEach(function(f) {
+  addForkAssertions(f, [ O(0, 1), O(1, 0), O(1, 2), O(2, 1) ]);
 });
