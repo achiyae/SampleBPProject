24478f38a13012c3a82ce6757174ea6cac85d102
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2022-06-14 19:36:00 +0300

    Fix TTT

--- src/main/resources/TTT.js
+++ src/main/resources/TTT.js
@@ -120,76 +120,28 @@ lines.forEach(function (l) {
   addLineBThreads(l.map(c => X(c.x, c.y)), l.map(c => O(c.x, c.y)))
 })
 
-function addFork22PermutationBthreads(fork) {
-  // Player O strategy to prevent the Fork22 of player X
-  bp.registerBThread('PreventFork22X', function () {
-    bp.sync({ waitFor: fork })
-    bp.sync({ waitFor: fork })
-    bp.sync({ request: [O(2, 2), O(0, 2), O(2, 0)] }, 30)
+function addForkBThread(X,O) {
+  bp.registerBThread('PreventFork', function () {
+    bp.sync({ waitFor: X })
+    bp.sync({ waitFor: X })
+    bp.sync({ request: O }, 30)
   })
 }
 
-function addFork02PermutationBthreads(fork) {
-  // Player O strategy to prevent the Fork02 of player X
-  bp.registerBThread('PreventFork02X', function () {
-    bp.sync({ waitFor: fork })
-    bp.sync({ waitFor: fork })
-    bp.sync({ request: [O(0, 2), O(0, 0), O(2, 2)] }, 30)
+var forks22 = { X: [[{ x: 1, y: 2 }, { x: 2, y: 0 }], [{ x: 2, y: 1 }, { x: 0, y: 2 }], [{ x: 1, y: 2 }, { x: 2, y: 1 }]],O:[O(2, 2), O(0, 2), O(2, 0)]}
+var forks02 = { X: [[{ x: 1, y: 2 }, { x: 0, y: 0 }], [{ x: 0, y: 1 }, { x: 2, y: 2 }], [{ x: 1, y: 2 }, { x: 0, y: 1 }]],O:[O(0, 2), O(0, 0), O(2, 2)]}
+var forks20 = { X: [[{ x: 1, y: 0 }, { x: 2, y: 2 }], [{ x: 2, y: 1 }, { x: 0, y: 0 }], [{ x: 2, y: 1 }, { x: 1, y: 0 }]],O:[O(2, 0), O(0, 0), O(2, 2)]}
+var forks00 = { X: [[{ x: 0, y: 1 }, { x: 2, y: 0 }], [{ x: 1, y: 0 }, { x: 0, y: 2 }], [{ x: 0, y: 1 }, { x: 1, y: 0 }]],O:[O(0, 0), O(0, 2), O(2, 0)] }
+var forksdiag = { X: [[{ x: 0, y: 2 }, { x: 2, y: 0 }], [{ x: 0, y: 0 }, { x: 2, y: 2 }]],O:[O(0, 1), O(1, 0), O(1, 2), O(2, 1)]}
+
+var forks = forks22.concat(forks02, forks20, forks00, forksdiag)
+
+forks.forEach(function (f) {
+  f.X.forEach(function (X) {
+    addForkBThread(X.map(c => X(c.x, c.y)),f.O.map(c => O(c.x, c.y)))
   })
-}
-
-function addFork20PermutationBthreads(fork) {
-  bp.registerBThread('PreventFork20X', function () {
-      bp.sync({ waitFor: fork })
-      bp.sync({ waitFor: fork })
-
-      bp.sync({ request: [O(2, 0), O(0, 0), O(2, 2)] }, 30)
-    }
-  )
-}
-
-function addFork00PermutationBthreads(fork) {
-  bp.registerBThread('PreventFork00X', function () {
-    bp.sync({ waitFor: fork })
-    bp.sync({ waitFor: fork })
-    bp.sync({ request: [O(0, 0), O(0, 2), O(2, 0)] }, 30)
-  })
-}
-
-function addForkdiagPermutationBthreads(fork) {
-  bp.registerBThread('PreventForkdiagX', function () {
-    bp.sync({ waitFor: fork })
-    bp.sync({ waitFor: fork })
-    bp.sync({ request: [O(0, 1), O(1, 0), O(1, 2), O(2, 1)] }, 30)
-  })
-}
-
-var forks22 = [[{ x: 1, y: 2 }, { x: 2, y: 0 }], [{ x: 2, y: 1 }, { x: 0, y: 2 }], [{ x: 1, y: 2 }, { x: 2, y: 1 }]]
-var forks02 = [[{ x: 1, y: 2 }, { x: 0, y: 0 }], [{ x: 0, y: 1 }, { x: 2, y: 2 }], [{ x: 1, y: 2 }, { x: 0, y: 1 }]]
-var forks20 = [[{ x: 1, y: 0 }, { x: 2, y: 2 }], [{ x: 2, y: 1 }, { x: 0, y: 0 }], [{ x: 2, y: 1 }, { x: 1, y: 0 }]]
-var forks00 = [[{ x: 0, y: 1 }, { x: 2, y: 0 }], [{ x: 1, y: 0 }, { x: 0, y: 2 }], [{ x: 0, y: 1 }, { x: 1, y: 0 }]]
-
-var forksdiag = [[{ x: 0, y: 2 }, { x: 2, y: 0 }], [{ x: 0, y: 0 }, { x: 2, y: 2 }]]
-
-forks22.forEach(function (f) {
-  addFork22PermutationBthreads(f.map(c => X(c.x, c.y)))
 })
 
-forks02.forEach(function (f) {
-  addFork02PermutationBthreads(f.map(c => X(c.x, c.y)))
-})
-
-forks20.forEach(function (f) {
-  addFork20PermutationBthreads(f.map(c => X(c.x, c.y)))
-})
-
-forks00.forEach(function (f) {
-  addFork00PermutationBthreads(f.map(c => X(c.x, c.y)))
-})
-
-forksdiag.forEach(function (f) {
-  addForkdiagPermutationBthreads(f.map(c => X(c.x, c.y)))
-})
 
 // Preference to put O on the center
 bp.registerBThread('Center', function () {
