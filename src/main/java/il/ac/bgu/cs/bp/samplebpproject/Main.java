80fdafbc7d06cc61ebfa3b01014385fd5df81d7d
Achiya Elyasaf <10044875+achiyae@users.noreply.github.com>
2023-04-22 22:45:10 +0300

    refactoring

--- src/main/java/il/ac/bgu/cs/bp/samplebpproject/Main.java
+++ src/main/java/il/ac/bgu/cs/bp/samplebpproject/Main.java
@@ -6,78 +6,66 @@ import il.ac.bgu.cs.bp.bpjs.context.ContextBProgram;
 import il.ac.bgu.cs.bp.bpjs.execution.BProgramRunner;
 import il.ac.bgu.cs.bp.bpjs.execution.listeners.PrintBProgramRunnerListener;
 import il.ac.bgu.cs.bp.bpjs.model.BProgram;
-import il.ac.bgu.cs.bp.bpjs.model.eventselection.PrioritizedBSyncEventSelectionStrategy;
-import il.ac.bgu.cs.bp.statespacemapper.MapperResult;
-import il.ac.bgu.cs.bp.statespacemapper.StateSpaceMapper;
-import il.ac.bgu.cs.bp.statespacemapper.jgrapht.exports.DotExporter;
-import il.ac.bgu.cs.bp.statespacemapper.jgrapht.exports.Exporter;
-import il.ac.bgu.cs.bp.statespacemapper.jgrapht.exports.GoalExporter;
-import org.jgrapht.GraphPath;
+import il.ac.bgu.cs.bp.statespacemapper.SpaceMapperCliRunner;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.text.MessageFormat;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.util.Optional;
 
-public class Main {
-  private String name;
-  private BProgram bprog;
-  private SampleDomain example = null;
-
+public class Main extends SpaceMapperCliRunner {
   public static void main(String[] args) throws Exception {
     var main = new Main();
+    BProgram bprog;
+    String name;
+    Optional<SampleDomain> sampleDomain = Optional.empty();
     System.out.println("// start");
 
     // load a bprogram of a sample domain
-    main.loadSampleDomain(SampleDomain.HotCold);
+    sampleDomain = Optional.of(SampleDomain.HotCold);
+    bprog = loadSampleDomain(sampleDomain.get());
+    name = sampleDomain.get().name;
 
     // alternatively, load a bprogram from resources
-    /* var resources = args.length == 0 ? new String[]{"HelloBPjsWorld.js"} : args;
-    main.loadResources(resources);*/
+    /*
+    var resources = args.length == 0 ? new String[]{"HelloBPjsWorld.js"} : args;
+    bprog = cli.getBProgram(resources);
+    name = bprog.getName();
+    // You can use a different EventSelectionStrategy, for example:
+    bprog.setEventSelectionStrategy(new PrioritizedBSyncEventSelectionStrategy());
+    */
 
     // run the program:
-    // main.runProgram();
+//     main.runProgram(bprog, sampleDomain);
 
     // verify the program:
-    // main.verifyProgram();
+//     main.verifyProgram(bprog, sampleDomain);
 
     // map the state space of the program:
-    var map = main.mapSpace();
+    var map = main.mapSpace(bprog);
 
     // write the graph to files:
-    main.writeGraphs(map);
+    main.exportSpace(name, map);
 
     // Generates a compressed file with all possible paths. Could be huge.
-    // writeCompressedPaths(name + ".csv", null, res, "exports");
+//    main.writeCompressedPaths(name + ".csv", null, map, "exports");
 
     System.out.println("// done");
   }
 
-  private void loadSampleDomain(SampleDomain domain) {
-    example = domain;
-    this.bprog = new ContextBProgram(example.getResourcesNames());
-    example.initializeBProg(bprog);
-    this.name = example.name;
+  private static BProgram loadSampleDomain(SampleDomain domain) {
+    var bprog = new ContextBProgram(domain.getResourcesNames());
+    domain.initializeBProg(bprog);
+    return bprog;
   }
 
-  private void loadResources(String[] resources) {
-    // To load non-sample program (i.e., for example the program <Project>/src/main/resources/HelloBPjsWorld.js)
-    this.bprog = new ContextBProgram(resources); // you can add more files
-    this.name = bprog.getName();
-
-    // You can use a different EventSelectionStrategy, for example:
-    bprog.setEventSelectionStrategy(new PrioritizedBSyncEventSelectionStrategy());
-  }
-
-  private void verifyProgram() throws IOException {
-    if (example != null) {
-      example.addVerificationResources(bprog);
-    }
+  private void verifyProgram(BProgram bprog, Optional<SampleDomain> sampleDomain) {
+    sampleDomain.ifPresent(sample -> {
+      try {
+        sample.addVerificationResources(bprog);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    });
     var vfr = new DfsBProgramVerifier();
     vfr.setMaxTraceLength(2000);
     vfr.setProgressListener(new PrintDfsVerifierListener());
@@ -100,84 +88,10 @@ public class Main {
     }
   }
 
-  private void runProgram() {
+  private static void runProgram(BProgram bprog, Optional<SampleDomain> sampleDomain) {
     var rnr = new BProgramRunner(bprog);
-    if (example != null) {
-      example.initializeRunner(rnr);
-    } else {
-      rnr.addListener(new PrintBProgramRunnerListener());
-    }
+    sampleDomain.ifPresentOrElse(
+      sample -> sample.initializeRunner(rnr), () -> rnr.addListener(new PrintBProgramRunnerListener()));
     rnr.run();
-  }
-
-  private MapperResult mapSpace() throws Exception {
-    if (example != null) {
-      example.addVerificationResources(bprog);
-    }
-    StateSpaceMapper mpr = new StateSpaceMapper();
-    var res = mpr.mapSpace(bprog);
-    System.out.println("// completed mapping the states graph");
-    System.out.println(res.toString());
-    return res;
-  }
-
-  private Exporter formatExporter(Exporter e) {
-//    e.setVertexAttributeProvider(mapperVertex -> Map.of());
-    return e;
-  }
-
-  private void writeGraphs(MapperResult res) throws IOException {
-    System.out.println("// Export to GraphViz...");
-    var outputDir = "exports";
-    var path = Paths.get(outputDir, name + ".dot").toString();
-    formatExporter(new DotExporter(res, path, name)).export();
-
-    System.out.println("// Export to GOAL...");
-    boolean simplifyTransitions = true;
-    path = Paths.get(outputDir, name + ".gff").toString();
-    var goalExporter = new GoalExporter(res, path, name, simplifyTransitions);
-    formatExporter(goalExporter).export();
-  }
-
-  private void writeCompressedPaths(String csvFileName, Integer maxPathLength, MapperResult res, String outputDir) throws IOException {
-    System.out.println("// Generating paths...");
-    var allDirectedPathsAlgorithm = res.createAllDirectedPathsBuilder()
-      .setSimplePathsOnly(maxPathLength == null)
-      .setIncludeReturningEdgesInSimplePaths(maxPathLength == null)
-      .setLongestPathsOnly(false)
-      .setMaxPathLength(maxPathLength)
-      .build();
-    var graphPaths = allDirectedPathsAlgorithm.getAllPaths();
-
-    int maxLength = graphPaths.parallelStream().map(GraphPath::getLength).max(Integer::compareTo).orElse(0);
-    System.out.println("// Number of paths = " + graphPaths.size());
-    System.out.println("// Max path length = " + maxLength);
-
-    System.out.println("// Writing paths...");
-    try (var fos = new FileOutputStream(Paths.get(outputDir, csvFileName) + ".zip");
-         var zipOut = new ZipOutputStream(fos)) {
-      var zipEntry = new ZipEntry(csvFileName);
-      zipOut.putNextEntry(zipEntry);
-      zipOut.setLevel(9);
-      MapperResult.GraphPaths2BEventPaths(graphPaths)
-        .parallelStream()
-        .map(l -> l.stream()
-          .map(e -> {
-            if (e.name.equals("X") || e.name.equals("O")) {
-              return e.name + "(" + ((Map<String, Object>) e.maybeData).get("row") + "," + ((Map<String, Object>) e.maybeData).get("col") + ")";
-            } else return e.name;
-          })
-//            .filter(s -> !List.of("KeepDown", "ClosingRequest", "OpeningRequest").contains(s))
-          .collect(Collectors.joining(",", "", "\n")))
-        .distinct()
-        .sorted()
-        .forEachOrdered(s -> {
-          try {
-            zipOut.write(s.getBytes(StandardCharsets.UTF_8));
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        });
-    }
   }
 }
